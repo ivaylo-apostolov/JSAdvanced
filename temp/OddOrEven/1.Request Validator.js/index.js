ac56677b3ce669f93fcdda8eefb40d9b05c09229
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-10-18 19:03:48 +0300

    unit test 1

---
+++ temp/OddOrEven/1.Request Validator.js/index.js
@@ -0,0 +1,18 @@
+function solve(input) {
+    let keys = Object.keys(input);
+
+    function checkAllProps(keys){
+         let haveAllElements = keys.find(x => x === 'method'
+        || x === 'uri'
+        || x === 'version'
+        || x === 'message')
+}
+
+solve(
+    {
+        method: 'GET',
+        uri: 'svn.public.catalog',
+        version: 'HTTP/1.1',
+        message: ''
+    }
+)
